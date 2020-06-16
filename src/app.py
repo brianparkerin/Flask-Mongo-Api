1dd46b7117614eebab54486590bb47b10b5ad9bd
Brian <brianparkerin@gmail.com>
2020-06-16 13:14:48 +0200

    first commit

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from bson import json_util
 from bson.objectid import ObjectId
 
 
-# AQUI INICIA NUESTRA - CRUD API
+# AQUI INICIA NUESTRA - CRUD REST-API
 # DataBase Connection with Flas_PyMongo Client
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://brian:123@cluster0-fpdvj.mongodb.net/flask-crud-api?retryWrites=true&w=majority"
--- src/index.py
+++ src/index.py
@@ -1,8 +1,4 @@
-from flask import Flask, render_template
-
-
-
-# SIMPLE RUTA API...
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -10,8 +6,45 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/user')
+def user():
+    return render_template('user.html')
 
+@app.route('/user/sing-in')
+def login():
+    return render_template('login.html')
 
+@app.route('/user/sing-up')
+def register():
+    return render_template('register.html')
 
-if __name__ == "__main__":
+@app.route('/sing-in')
+def signing():
+    return render_template('login.html')
+
+@app.route('/sing-up')
+def signup():
+    return render_template('register.html')
+
+@app.route('/business')
+def business():
+    return render_template('business.html')
+
+@app.route('/explore')
+def explore():
+    return render_template('explore.html')
+
+@app.route('/docs')
+def documentation():
+    return render_template('documentation.html')
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+if __name__== '__main__':
     app.run(debug=True)
